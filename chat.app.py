3a27ce8cc54f40cba85704d2b22ce668869352c2
djecon <djecon@icloud.com>
2023-08-29 11:30:03 -0700

    Update chat.app.py
    removed menu options

--- chat.app.py
+++ chat.app.py
@@ -4,12 +4,7 @@ import datetime
 import pytz
 
 iconimage = 'an-2050-dark-circle-logo-favicon.png'
-st.set_page_config(page_title="A&N Chatbot", page_icon=iconimage,
-        layout="wide",
-        initial_sidebar_state="auto",
-        menu_items={
-        'Documentation': 'https://documentation.softwareag.com/natural/nat921mf/webhelp/natmf-webhelp/overview.htm',
-        'About': "# This is an *extremely* cool app!"})
+st.set_page_config(page_title="A&N Chatbot", page_icon=iconimage)
 
 def query(payload):
     response = requests.post(API_URL, json=payload)
