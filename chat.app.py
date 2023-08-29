c4a9b2cd4769cad2200028e19b1916331fdcf750
djecon <djecon@icloud.com>
2023-08-28 22:43:26 -0700

    Update chat.app.py
    added time check

--- chat.app.py
+++ chat.app.py
@@ -1,41 +1,49 @@
 import streamlit as st
 import requests
+import datetime
+import pytz
 
 def query(payload):
     response = requests.post(API_URL, json=payload)
     return response.json()
+
+def is_valid_time():
+    eastern = pytz.timezone('US/Eastern')
+    current_time = datetime.datetime.now(eastern).time()
+    print(current_time)
+    return current_time < datetime.time(8, 0) or current_time >= datetime.time(20, 0)
 
 API_URL = "http://ec2-3-83-217-17.compute-1.amazonaws.com:3000/api/v1/prediction/6ce36d53-ed90-4759-877b-83aedadb617b"
 # API_URL = "http://localhost:3000/api/v1/prediction/575f6659-23eb-4ccb-885c-d2d3632fb976"
 st.title("Natural & Adabas Chatbot")
 st.text("Based on 29 PDF guides. This app is only available between 8am - 8pm Eastern")
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if not is_valid_time():
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# React to user input
-if prompt := st.chat_input("What is your question about Natural & Adabas?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    # React to user input
+    if prompt := st.chat_input("What is your question about Natural & Adabas?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response =  query({
-        "question": prompt,
-    })
+        response = query({
+            "question": prompt,
+        })
 
-#    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
-
-
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+else:
+    st.error("Sorry, this feature is only available between 8am and 8pm Eastern Standard Time.")
 
