4f3a609a8d26e3be282b3c8575f7feca17349b27
djecon <djecon@icloud.com>
2023-08-29 08:23:48 -0700

    Update chat.app.py
    added a logo

--- chat.app.py
+++ chat.app.py
@@ -15,6 +15,9 @@ def is_valid_time():
 
 API_URL = "http://44.206.234.21:3000/api/v1/prediction/6ce36d53-ed90-4759-877b-83aedadb617b"
 # API_URL = "http://localhost:3000/api/v1/prediction/575f6659-23eb-4ccb-885c-d2d3632fb976"
+
+logo_image = 'an-2050+light-horizontal-logo.svg'
+st.image(logo_image, width=100)
 st.title("Natural & Adabas Chatbot")
 st.text("Based on 29 PDF guides. This app is only available between 8am - 5pm Pacific")
 
