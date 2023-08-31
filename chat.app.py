ac0c5985104ed3c6292bdafb35f82d01a9198ce9
djecon <djecon@icloud.com>
2023-08-31 05:21:32 -0700

    Update chat.app.py

--- chat.app.py
+++ chat.app.py
@@ -16,7 +16,7 @@ def is_valid_time():
     print(current_time)
     return current_time < datetime.time(8, 0) or current_time >= datetime.time(17, 0)
 
-API_URL = "http://3.88.226.187:3000/api/v1/prediction/6ce36d53-ed90-4759-877b-83aedadb617b"
+API_URL = "http://107.23.95.203:3000/api/v1/prediction/6ce36d53-ed90-4759-877b-83aedadb617b"
 # API_URL = "http://localhost:3000/api/v1/prediction/575f6659-23eb-4ccb-885c-d2d3632fb976"
 
 logo_image = 'an-2050-light-horizontal-logo.png'
@@ -27,6 +27,9 @@ st.markdown(favicon_html, unsafe_allow_html=True)
 st.image(logo_image)
 st.title("Chatbot")
 st.text("Based on 49 PDF guides. This app is only available between 8am - 5pm Pacific")
+st.text("The following information is provided by an AI-powered system and should be used for informational purposes only. 
+        While the system strives to provide accurate and relevant responses, it may not always reflect human expertise or account 
+        for specific contexts. Users are advised to verify the information independently and consult with domain experts if needed.")
 
 if not is_valid_time():
     # Initialize chat history
